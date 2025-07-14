9472e6fa26c548d54a3a46a5a0516e3fe6f87638
lien-nguyen <moonlierose@gmail.com>
2025-07-14 15:58:26 +0200

    Setup markdown function for blog

--- build2teach/blog/views.py
+++ build2teach/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+import markdown 
+from django.utils.safestring import mark_safe
 
-# Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
+    for post in posts:
+        post.content_html = mark_safe(markdown.markdown(post.content, extensions=['fenced_code', 'codehilite']))
     return render(request, 'blog/post_list.html', {'posts': posts})
